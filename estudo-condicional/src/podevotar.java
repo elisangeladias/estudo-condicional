2ae9fe01a3a45ccfe54fc886530e54ec8eb856d4
elisangeladias <elisangelacwb@hotmail.com>
2024-08-09 15:46:43 -0300

    exemplo if - else

---
+++ estudo-condicional/src/podevotar.java
@@ -0,0 +1,25 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import javax.swing.JOptionPane;
+
+public class podevotar {
+
+	public static void main(String[] args) {
+			String anoNascimento = JOptionPane.showInputDialog("Digite seu ano de nascimento");
+			int ano = Integer.parseInt(anoNascimento);
+			Calendar cal = GregorianCalendar.getInstance();
+			int podeVotar = cal.get(Calendar.YEAR) - ano;
+			
+			if(podeVotar >= 16 ) {
+				
+				JOptionPane.showMessageDialog(null, "Pde votar");
+				
+			}
+			
+			
+			
+
+		}
+
+	}
